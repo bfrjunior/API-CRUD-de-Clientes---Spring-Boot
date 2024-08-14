90b619c91524189b6ae9ffea716ecfb2538ec5a2
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-13 23:48:55 -0300

    feat: cliente route

--- src/main/java/br/com/clientes/api/controller/Controller.java
+++ src/main/java/br/com/clientes/api/controller/Controller.java
@@ -1,14 +1,22 @@
 package br.com.clientes.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.clientes.api.model.Cliente;
+import br.com.clientes.api.repository.ClienteRepository;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/")
-    public String teste() {
-        return "teste";
+    @Autowired
+    private ClienteRepository acao;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c) {
+        return acao.save(c);
     }
 
 }
